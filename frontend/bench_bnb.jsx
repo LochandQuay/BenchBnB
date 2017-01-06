dff11e2f821de7198d98ce815fcf35be8bca6a6a
Hope <hopewanroy@gmail.com>
2017-01-05 22:20:17 -0800

    Add basic frontend structure and DOM rendering

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { configureStore } from '/store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // const store = configureStore();
+  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, document.getElementById("root"));
+})
