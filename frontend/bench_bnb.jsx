ad39f5c7e0a6ae3e3d46a869c87685f4548626a6
Hope <hopewanroy@gmail.com>
2017-01-06 01:15:00 -0800

    Add store and configureStore

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { configureStore } from '/store/store';
+import { configureStore } from './store/store';
 
 // FOR TESTING ONLY. DELETE WHEN COMPLETE
 import * as SessionAPIUtil from './util/session_api_util';
-
-window.signup = SessionAPIUtil.signup;
-window.login = SessionAPIUtil.login;
-window.logout = SessionAPIUtil.logout;
+window.store = configureStore();
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const store = configureStore();
+  const store = configureStore();
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, document.getElementById('root'));
 });
